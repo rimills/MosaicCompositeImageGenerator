ee0b3f45623ebf5bf27d255467c35b9631d50858
DylanKane <DylanKane@computing.dundee.ac.uk>
2016-11-10 16:38:09 +0000

    similarity function and test images

--- tester.m
+++ tester.m
@@ -1,2 +1,20 @@
+%tester function to run other scripts.
 function tester()
-    [r,g,b] = average_image 'Image\natural_training\out_natural_1k\sun_aachaucttrrkdicr.jpg'
+    %colourAnalyse();
+    sim();
+    
+    function colourAnalyse()
+        image = 'Image\own_tests\green.jpg';
+        [r,g,b] = average_image(image);
+        r
+        g
+        b
+    end
+
+    function sim()
+        first_image = 'Image\own_tests\green.jpg';
+        second_image = 'Image\own_tests\green.jpg';
+        
+        similarity(first_image, second_image);
+    end
+end
